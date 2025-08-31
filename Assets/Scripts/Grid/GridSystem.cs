dd181b0931f0b71dc15dcddd183931a314823fb1
Denis Tarulli <132310271+DenisTarulli@users.noreply.github.com>
2025-08-31 09:44:25 -0300

    Level Grid & Cinemachine Camera Controller
    Note: Added comentaries to some grid related functions

---
+++ Assets/Scripts/Grid/GridSystem.cs
@@ -0,0 +1,89 @@
+using UnityEngine;
+
+public class GridSystem
+{
+    private int width;
+    private int height;
+    private float cellSize;
+    private GridObject[,] gridObjectArray;
+
+    /// <summary>
+    /// Constructor which creates a grid of size <see href="width"/> by <see href="height"/>
+    /// where each cell of size <see href="cellSize"/> has a new <see cref="GridObject"/> asigned 
+    /// and added to the <see href="gridObjectArray"/> of this class
+    /// </summary>
+    /// <param name="width">Width of the grid</param>
+    /// <param name="height">Height of the grid</param>
+    /// <param name="cellSize">Size of the cell</param>
+    public GridSystem(int width, int height, float cellSize)
+    {
+        this.width = width;
+        this.height = height;
+        this.cellSize = cellSize;
+
+        gridObjectArray = new GridObject[width, height];
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int z = 0; z < height; z++)
+            {
+                GridPosition gridPosition = new GridPosition(x, z);
+                gridObjectArray[x, z] = new GridObject(this, gridPosition);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Returns the world position of the given grid position (x, z index)
+    /// </summary>
+    /// <param name="gridPosition"></param>
+    /// <returns></returns>
+    public Vector3 GetWorldPosition(GridPosition gridPosition)
+    {
+        return new Vector3(gridPosition.x, 0, gridPosition.z) * cellSize;
+    }
+
+    /// <summary>
+    /// Returns the grid position (x, z index) of the given world position
+    /// </summary>
+    /// <param name="worldPosition">World position</param>
+    /// <returns></returns>
+    public GridPosition GetGridPosition(Vector3 worldPosition)
+    {
+        return new GridPosition(
+            Mathf.RoundToInt(worldPosition.x / cellSize),    
+            Mathf.RoundToInt(worldPosition.z / cellSize)
+        );
+    }
+
+    /// <summary>
+    /// Instantiates a debug prefab on each grid object (cell) of the grid
+    /// </summary>
+    /// <param name="debugPrefab">Debug prefab to instantiate</param>
+    public void CreateDebugObjects(Transform debugPrefab)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int z = 0; z < height; z++)
+            {
+                GridPosition gridPosition = new GridPosition(x, z);
+
+                Transform debugTransform = GameObject.Instantiate(debugPrefab, GetWorldPosition(gridPosition), Quaternion.identity);
+                GridDebugObject gridDebugObject = debugTransform.GetComponent<GridDebugObject>();
+                gridDebugObject.SetGridObject(GetGridObject(gridPosition));
+            }
+        }
+    }
+
+    /// <summary>
+    /// Returns the corresponding grid object of the given grid position
+    /// </summary>
+    /// <param name="gridPosition">Grid position (x, z index)</param>
+    /// <returns></returns>
+    public GridObject GetGridObject(GridPosition gridPosition)
+    {
+        return gridObjectArray[gridPosition.x, gridPosition.z];
+    }
+}
+
+
--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -7,10 +7,17 @@ public class Unit : MonoBehaviour
     [SerializeField] private Animator unitAnimator;
 
     private Vector3 targetPosition;
+    private GridPosition gridPosition;
 
     private void Awake()
     {
         targetPosition = transform.position;
+    }
+
+    private void Start()
+    {
+        gridPosition = LevelGrid.Instance.GetGridPosition(transform.position);
+        LevelGrid.Instance.AddUnitAtGridPosition(gridPosition, this);
     }
 
     private void Update()
@@ -33,6 +40,15 @@ public class Unit : MonoBehaviour
         else
         {
             unitAnimator.SetBool(UNIT_ISWALKING, false);
+        }
+
+        GridPosition newGridPosition = LevelGrid.Instance.GetGridPosition(transform.position);
+
+        if (newGridPosition != gridPosition)
+        {
+            // Unit changed GridPosition
+            LevelGrid.Instance.UnitMovedGridPosition(this, gridPosition, newGridPosition);
+            gridPosition = newGridPosition;
         }
     }
 
