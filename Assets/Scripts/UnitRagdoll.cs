9d6d1c49c930fb4b4a7be8f264add663dd87de4c
Denis Tarulli <132310271+DenisTarulli@users.noreply.github.com>
2025-09-02 13:09:52 -0300

    Unit Ragdoll

---
+++ Assets/Scripts/UnitRagdoll.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitRagdoll : MonoBehaviour
+{
+    [SerializeField] private Transform ragdollRootBone;
+
+    public void Setup(Transform originalRootBone)
+    {
+        MatchAllChildTransforms(originalRootBone, ragdollRootBone);
+
+        ApplyExlosionToRagdoll(ragdollRootBone, 400f, transform.position, 10f);
+    }
+
+    private void MatchAllChildTransforms(Transform root, Transform clone)
+    {
+        foreach (Transform child in root)
+        {
+            Transform cloneChild = clone.Find(child.name);
+
+            if (cloneChild != null)
+            {
+                cloneChild.SetPositionAndRotation(child.position, child.rotation);
+
+                MatchAllChildTransforms(child, cloneChild);
+            }
+        }
+    }
+
+    private void ApplyExlosionToRagdoll(Transform root, float explosionForce, Vector3 explosionPosition, float explosionRange)
+    {
+        foreach (Transform child in root)
+        {
+            if (child.TryGetComponent<Rigidbody>(out Rigidbody childRigidBody))
+            {
+                childRigidBody.AddExplosionForce(explosionForce, explosionPosition, explosionRange);
+
+                ApplyExlosionToRagdoll(child, explosionForce, explosionPosition, explosionRange);
+            }
+        }
+    }
+}
