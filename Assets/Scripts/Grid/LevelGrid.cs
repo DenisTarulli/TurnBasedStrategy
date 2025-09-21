ed0062dee7207195dd225538f6a29634f33ef2cc
Denis Tarulli <132310271+DenisTarulli@users.noreply.github.com>
2025-09-21 17:56:27 -0300

    Pathfinding obstacles

--- Assets/Scripts/Grid/LevelGrid.cs
+++ Assets/Scripts/Grid/LevelGrid.cs
@@ -10,6 +10,9 @@ public class LevelGrid : MonoBehaviour
 
     [SerializeField] private Transform gridDebugObjectPrefab;
 
+    [SerializeField] private int width;
+    [SerializeField] private int height;
+    [SerializeField] private float cellSize;
     private GridSystem<GridObject> gridSystem;
 
     private void Awake()
@@ -23,10 +26,15 @@ public class LevelGrid : MonoBehaviour
 
         Instance = this;
 
-        gridSystem = new GridSystem<GridObject>(10, 10, 2f, 
+        gridSystem = new GridSystem<GridObject>(width, height, cellSize, 
             (GridSystem<GridObject> g, GridPosition gridPosition) => new GridObject(g, gridPosition));
 
         //gridSystem.CreateDebugObjects(gridDebugObjectPrefab);
+    }
+
+    private void Start()
+    {
+        Pathfinding.Instance.Setup(width, height, cellSize);
     }
 
     public void AddUnitAtGridPosition(GridPosition gridPosition, Unit unit)
