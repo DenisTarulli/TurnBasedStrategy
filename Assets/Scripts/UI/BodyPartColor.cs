58c1963c56394b7c37d650e4c3d85a46c81f023d
IgnacioMartinezInfante <ignacio.martinezinfante@gmail.com>
2026-02-11 20:15:56 -0300

    Cambio de colores wizard primera parte

---
+++ Assets/Scripts/UI/BodyPartColor.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class BodyPartColor : MonoBehaviour
+{
+    public enum BodyPartType
+    {
+        Head,
+        Boots,
+        LeftArm,
+        RightArm,
+        Shoulders,
+        Tunic
+    }
+
+    [SerializeField] private BodyPartType bodyPartType;
+
+    private SkinnedMeshRenderer skinnedMeshRenderer;
+    private MaterialPropertyBlock mpb;
+
+    private void Awake()
+    {
+        skinnedMeshRenderer = GetComponent<SkinnedMeshRenderer>();
+        mpb = new MaterialPropertyBlock();
+    }
+
+    public BodyPartType GetBodyPartType()
+    {
+        return bodyPartType;
+    }
+
+    public void SetColor(Color color)
+    {
+        skinnedMeshRenderer.GetPropertyBlock(mpb);
+        mpb.SetColor("_BaseColor", color);
+        skinnedMeshRenderer.SetPropertyBlock(mpb);
+    }
+}
---
+++ Assets/Scripts/UI/WizardVisualManager.cs
@@ -0,0 +1,56 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WizardVisualManager : MonoBehaviour
+{
+    [SerializeField] private List<BodyPartColor> bodyParts;
+
+    private Dictionary<BodyPartColor.BodyPartType, BodyPartColor> bodyPartDictionary;
+
+    private int paintedPartsIndex = 0;
+
+    private readonly BodyPartColor.BodyPartType[] paintOrder =
+    {
+        BodyPartColor.BodyPartType.LeftArm,
+        BodyPartColor.BodyPartType.Boots,
+        BodyPartColor.BodyPartType.Head,
+        BodyPartColor.BodyPartType.RightArm,
+        BodyPartColor.BodyPartType.Shoulders,
+        BodyPartColor.BodyPartType.Tunic
+    };
+
+    private void Awake()
+    {
+        bodyPartDictionary = new Dictionary<BodyPartColor.BodyPartType, BodyPartColor>();
+
+        foreach (var part in bodyParts)
+        {
+            bodyPartDictionary[part.GetBodyPartType()] = part;
+        }
+    }
+
+    private void Start()
+    {
+        PlayerStats.Instance.OnSpeedChanged += (_, __) => PaintNextPart(Color.green);
+        PlayerStats.Instance.OnHealthChanged += (_, __) => PaintNextPart(Color.red);
+        PlayerStats.Instance.OnEnergyChanged += (_, __) => PaintNextPart(Color.yellow);
+        PlayerStats.Instance.OnResistanceChanged += (_, __) => PaintNextPart(Color.blue);
+        PlayerStats.Instance.OnPowerChanged += (_, __) => PaintNextPart(new Color(0.6f, 0f, 0.8f));
+    }
+
+    private void PaintNextPart(Color color)
+    {
+        if (paintedPartsIndex >= paintOrder.Length)
+        {
+            return;
+        }
+
+        var partToPaint = paintOrder[paintedPartsIndex];
+
+        if (bodyPartDictionary.TryGetValue(partToPaint, out var bodyPart))
+        {
+            bodyPart.SetColor(color);
+            paintedPartsIndex++;
+        }
+    }
+}
